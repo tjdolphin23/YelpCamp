8e8ab8035bc03d7c6b340a221c07bb1757a24ec8
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-11 08:09:44 -0700

    added post route, form to add new campground

--- app.js
+++ app.js
@@ -5,36 +5,40 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+		{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8444/7930246198_529eb829df.jpg"},
+		{name: "Indian Hill", image: "https://farm9.staticflickr.com/8010/7436786986_9972800b37.jpg"},
+		{name: "Big Bear", image: "https://farm9.staticflickr.com/8182/7930033862_38c29cc27d.jpg"}
+	];
+
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 
-app.get("/campgrounds", function(req,res){
-	var campgrounds = [
-		{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8444/7930246198_529eb829df.jpg"},
-		{name: "Indian Hill", image: "https://farm9.staticflickr.com/8010/7436786986_9972800b37.jpg"},
-		{name: "Big Bear", image: "https://farm9.staticflickr.com/8182/7930033862_38c29cc27d.jpg"}
-	]	
+app.get("/campgrounds", function(req,res){	
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 
 app.post("/campgrounds", function(req, res){
 	//get date form & add to list
-
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image}
+	campgrounds.push(newCampground);
 	//redirect back to campgrounds page
+	res.redirect("/campgrounds");
 });
 
 
 app.get("/campgrounds/new", function(req, res){
 	res.render("new.ejs");
-})
+});
 
+//local port
 var PORT = process.env.PORT || 8080;
-
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
