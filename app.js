b57f8a12c84c76d5cb2a22cfc0f4ef89de8d123f
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-12 15:39:44 -0700

    set up schema, mongoose model, create campground into db, create find all, create campground

--- app.js
+++ app.js
@@ -3,14 +3,31 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-		{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8444/7930246198_529eb829df.jpg"},
-		{name: "Indian Hill", image: "https://farm9.staticflickr.com/8010/7436786986_9972800b37.jpg"},
-		{name: "Big Bear", image: "https://farm9.staticflickr.com/8182/7930033862_38c29cc27d.jpg"}
-	];
+//schema set up
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+// 	name: "Indian Hill",
+// 	image: "https://farm9.staticflickr.com/8010/7436786986_9972800b37.jpg"
+// }, function(err, campground) {
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log("New campground created!!!");
+// 		console.log(campground);
+// 	}
+// });
+
 
 
 app.get("/", function(req, res){
@@ -19,7 +36,14 @@ app.get("/", function(req, res){
 
 
 app.get("/campgrounds", function(req,res){	
-	res.render("campgrounds", {campgrounds: campgrounds});
+	//get all campgrounds from DB
+	Campground.find({}, function(err, allCampground){
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: allCampground});
+		}
+	})
 });
 
 
@@ -28,9 +52,15 @@ app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image}
-	campgrounds.push(newCampground);
-	//redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	//create a new campground
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err) {
+			console.log(err); 
+		} else {
+			//redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
 
