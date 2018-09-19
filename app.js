0b06ee9b5c1034f14d07b17d7b2ee5e1982b1c1b
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-19 10:29:56 -0700

    display comments on show page

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var seedDB = require("./seeds");
 
-
-seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-
+seedDB();
 
 app.get("/", function(req, res){
 	res.render("landing");
