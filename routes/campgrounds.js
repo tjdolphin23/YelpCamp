f436a3c3daf972bbcf0b29c9c2e664dddacf323b
Tom Dolphin <thedolphingroup1@gmail.com>
2018-10-03 10:33:18 -0700

    removed edit and delete buttons if current user != to creator of campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -69,7 +69,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 
 
 //UPDATE CAMPGROUND ROUTE
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
 	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
 		if(err){
 			res.redirect("/campgrounds");
@@ -80,7 +80,7 @@ router.put("/:id", function(req, res){
 });
 
 //DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
 	Campground.findByIdAndRemove(req.params.id, function(err){
 		if(err){
 			res.redirect("/campgrounds");
@@ -103,7 +103,7 @@ function checkCampgroundOwnership(req, res, next) {
 			if(err) {
 				res.redirect("back");
 			} else {
-				if(foundCampground.author.id.equal(req.user._id)){
+				if(foundCampground.author.id.equals(req.user._id)){
 					next();
 				} else {
 					res.redirect("back");
