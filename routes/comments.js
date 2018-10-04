71ba0093f7487ac9720a416f74da51952ffa81ce
Tom Dolphin <thedolphingroup1@gmail.com>
2018-10-04 10:11:33 -0700

    Created comment ownership middleware to prevent other users from editing or deleting comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,7 +41,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //COMMENTS EDIT ROUTE
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
   Comment.findById(req.params.comment_id, function(err, foundComment){
     if(err){
       res.redirect("back");
@@ -64,11 +64,33 @@ router.put("/:comment_id", function(req, res){
 
 //COMMENT DESTROY ROUTE
 router.delete("/:comment_id", function(req, res){
-  res.send("This is DESTROY route");
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
 });
 
-
-
+//CHECK COMMENT OWNERSHIP FOR UPDATING, DESTROY 
+function checkCommentOwnership(req, res, next) {
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err) {
+        res.redirect("back");
+      } else {
+        if(foundComment.author.id.equals(req.user._id)){
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
 
 
 //MIDDLEWARE
