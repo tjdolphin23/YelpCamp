3cd3169e2b53f561e0111cbce8b96e383e1788f0
Tom Dolphin <thedolphingroup1@gmail.com>
2018-10-15 14:26:38 -0700

    add review route, require routes, and add review index route

---
+++ routes/reviews.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var Campground = require("../models/campground");
+var Review = require("../models/review");
+var middleware = require("../middleware");
+
+
+// Reviews Index
+router.get("/", function (req, res) {
+    Campground.findById(req.params.id).populate({
+        path: "reviews",
+        options: {sort: {createdAt: -1}} // sorting the populated reviews array to show the latest first
+    }).exec(function (err, campground) {
+        if (err || !campground) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        res.render("reviews/index", {campground: campground});
+    });
+});
