f5b4a0f78d200fd8ba2890ce42d9c35cf6527d34
Tom Dolphin <thedolphingroup1@gmail.com>
2018-10-15 14:31:39 -0700

    create review calculator function

--- routes/reviews.js
+++ routes/reviews.js
@@ -122,3 +122,16 @@ router.delete("/:review_id", middleware.checkReviewOwnership, function (req, res
         });
     });
 });
+
+function calculateAverage(reviews) {
+    if (reviews.length === 0) {
+        return 0;
+    }
+    var sum = 0;
+    reviews.forEach(function (element) {
+        sum += element.rating;
+    });
+    return sum / reviews.length;
+}
+
+module.exports = router;
